a5d8db1fcff9ace6d207826d0cd9be875d1a3f8d
SCWinter259 <catungthongkhi2002@gmail.com>
2023-08-16 19:58:54 -0600

    added code to the backend

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";     // import the route
+
+const app = express(); // initialize the app
+
+// use express middleware to connect the route to the application
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); // limit is 30mb for images
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://scwinter:scwinter123123@cluster0.qqixjgx.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+// connect to the database
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// makes sure that we don't get any warnings in the console
+// mongoose.set("useFindAndModify", false); -> deprecated
---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getPosts, createPost } from "../controllers/posts.js";
+
+// set up the router
+const router = express.Router();
+
+// set up the routes
+// this is reached through localhost:5000/posts, 
+// because we add a prefix of /posts to all the routes here
+// Functionalities for the routes are defined in controllers folder
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router;
