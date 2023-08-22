f234fd5ccbdecd116780d2ea196558bdc940775a
SCWinter259 <catungthongkhi2002@gmail.com>
2023-08-21 22:22:30 -0600

    done login form ui

---
+++ client/src/components/Auth/Input.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+
+interface InputFieldProps {
+  name: string;
+  changeHandler: () => void;
+  label: string;
+  half?: boolean;
+  autoFocus?: boolean;
+  type?: string;
+  showPasswordHandler?: () => void;
+}
+
+export const Input: React.FC<InputFieldProps> = ({
+  name,
+  changeHandler,
+  label,
+  half,
+  autoFocus,
+  type,
+  showPasswordHandler,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={changeHandler}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={showPasswordHandler}>
+                      {type === "password" ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : undefined
+        }
+      />
+    </Grid>
+  );
+};
