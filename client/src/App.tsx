415143aa54a15c167c42df97184faf14d950dcb6
SCWinter259 <catungthongkhi2002@gmail.com>
2023-08-22 22:05:51 -0600

    done google button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,21 @@ import { Home } from "./components/Home/Home";
 import { Auth } from "./components/Auth/Auth";
 import { NavBar } from "./components/NavBar/NavBar";
 import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Container maxWidth="lg">
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
-        </Switch>
-      </Container>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
+      <BrowserRouter>
+        <Container maxWidth="lg">
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/auth" exact component={Auth} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
 };
 
