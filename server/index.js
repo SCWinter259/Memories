b48a38f2d350e07a84bd75da47e3be0374a5fa01
SCWinter259 <catungthongkhi2002@gmail.com>
2023-09-01 11:48:23 -0600

    changed env stuff

--- server/index.js
+++ server/index.js
@@ -22,10 +22,12 @@ app.get("/", (req, res) => res.send("APP IS RUNNING"));
 // it seems like we cannot do a process.env.PORT below because
 // the code below is in a promise
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL =
+  "mongodb+srv://scwinter:scwinter123123@cluster0.qqixjgx.mongodb.net/?retryWrites=true&w=majority";
 
 // connect to the database
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
